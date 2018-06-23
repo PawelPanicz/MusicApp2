ed5ad35b07479b7207c78b86d8de842448338301
W59737 <pasekit@gmail.com>
2018-06-23 10:40:56 +0200

    okładka utworu

--- app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.stud.musicapp.R;
 import com.example.stud.musicapp.api.ApiService;
+import com.example.stud.musicapp.api.Track;
 import com.example.stud.musicapp.api.Tracks;
 
 import retrofit2.Call;
@@ -43,10 +47,10 @@ public class SongDetailsActivity extends AppCompatActivity {
         ApiService.getService().getTrack(trackId).enqueue(new Callback<Tracks>(){
             @Override
             public void onResponse(@NonNull Call<Tracks> call, @NonNull Response<Tracks> response) {
-                Toast.makeText (
-                        SongDetailsActivity.this ,
-                        "Pobrano dane" , Toast.LENGTH_SHORT
-                ).show();
+                Tracks tracks = response.body();
+                if (tracks != null && tracks. track .size() > 0 ) {
+                    showData(tracks. track .get( 0 ));
+                }
             }
             @Override
             public void onFailure(@NonNull Call<Tracks> call, @NonNull Throwable t) {
@@ -65,7 +69,21 @@ public class SongDetailsActivity extends AppCompatActivity {
         return true;
     }
 
+    private void showData(Track track) {
+        TextView tvAlbum = findViewById(R.id. tvAlbum );
+        TextView tvGenre = findViewById(R.id. tvGenre );
+        TextView tvStyle = findViewById(R.id. tvStyle );
+        TextView tvDescription = findViewById(R.id. tvDescription );
+        tvAlbum.setText(track. strAlbum );
+        tvGenre.setText(track. strGenre );
+        tvStyle.setText(track. strStyle );
+        tvDescription.setText(track. strDescriptionEN );
 
+        if (track. strTrackThumb != null && !track. strTrackThumb .isEmpty()) {
+            ImageView ivThumb = findViewById(R.id. ivThumb );
+            Glide. with ( this ).load(track. strTrackThumb ).into(ivThumb);
+        }
+    }
 }
 
 
